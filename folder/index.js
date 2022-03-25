689b7dc2b28f8988cbd1badf70613c616acfeb6c
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-03-25 13:28:24 +0530

    add

---
+++ folder/index.js
@@ -0,0 +1,4 @@
+console.log("Vaishnavi")
+const colorSystem = require("colors");
+console.log(colorSystem);
+console.log('OMG Rainbows!'.rainbow);
---
+++ shelter/app.js
@@ -0,0 +1,5 @@
+console.log(" Success");
+// const vaish = require("nodemon");
+// console.log(vaish)
+
+console.log(" Update is successful")
